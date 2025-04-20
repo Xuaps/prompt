0f01a5001c5cb3149a192ae4e5816c9440ee427a
David Vílchez <dvilchez@xuaps.com>
2025-04-20 20:31:53 +0200

    fix: deploy file

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5173',
+    baseUrl: 'http://localhost:4173',
     supportFile: 'cypress/support/e2e.js',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
   },
